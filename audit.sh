565941db547f6616fb276a38fc87ec73ffcf4ad6
Stam He <stamhe@163.com>
2024-01-28 19:40:45 +0800

    renew

--- audit.sh
+++ audit.sh
@@ -1,51 +1,57 @@
 #!/bin/bash
-#Copyright (C) 2023 Web3Password PTE. LTD.(Singapore UEN: 202333030C) - All Rights Reserved 
-#Web3Password PTE. LTD.(Singapore UEN: 202333030C) holds the copyright of this file.
-#Unauthorized copying or redistribution of this file in binary forms via any medium is strictly prohibited.
-#For more information, please refer to https://www.web3password.com/web3password_license.txt
+# Copyright (C) 2024 Web3Password PTE. LTD.(Singapore UEN: 202333030C) - All Rights Reserved 
+# Web3Password PTE. LTD.(Singapore UEN: 202333030C) holds the copyright of this file.
+# Unauthorized copying or redistribution of this file in binary forms via any medium is strictly prohibited.
+# For more information, please refer to https://www.web3password.com/web3password_license.txt
 
+
+WORK_DIR=`pwd`
 
 SCRIPT_NAME="audit.sh"  
 SCRIPT_VERSION="1.0"  
 SCRIPT_AUTHOR="Web3Password"  
-SCRIPT_DESC="Interface Audit Scripts. Using this script, you can easily view the information encrypted in the interface, but of course, you must enter your own mnemonic to decrypt the view."  
-  
-# main() {  
-#   local mnemonic=""  
-#   local logstr=""  
-#   local logdir=""  
-  
-#   mnemonic="$1"  
-#   logstr="$2"  
-#   logdir="$3"  
-  
-#   if [[ -z "$logstr" && -z "$logdir" ]]; then  
-#     echo "Error: Please provide at least one of -logstr or -logdir."  
-#     exit 1  
-#   fi  
-  
-# #   echo "${mnemonic#*=}"
-  
-#   node Web3Password-Tools/web3password-api-audit.js "${mnemonic#*=}" "${logstr#*=}" "${logdir#*=}"
+SCRIPT_DESC="Audit Web3Password API Communication"  
 
-#   exit 0  
-# }  
-  
-# main "$1" "$2" "$3"
+nodejs_url="https://nodejs.org/dist/v20.11.0/node-v20.11.0-linux-x64.tar.xz"
+
+wget -c $nodejs_url -O /tmp/nodejs.tar.xz
+if [ $? -ne 0 ] ; then
+    echo "install nodejs failed..."
+    exit 1
+fi
+
+NODEJS_DIR=/tmp/nodejs
+mkdir -p ${NODEJS_DIR}
+tar xvf /tmp/nodejs.tar.xz -C ${NODEJS_DIR}/ --strip-components 1
+${NODEJS_DIR}/bin/node --version
+if [ $? -ne 0 ] ; then
+    echo "install nodejs failed..."
+    exit 2
+fi
+
+export PATH=$PATH:${NODEJS_DIR}/bin
+W3PTOOLS_DIR="${WORK_DIR}/Web3Password-Tools"
+
+cd ${W3PTOOLS_DIR} && npm install
+if [ $? -ne 0 ] ; then
+    echo "install npm failed..."
+    exit 3
+fi
+
 main() {  
     mnemonic="$1"  
-    data="$2"  
+    satis_logfile="$2"  
     if [ -z "$1" ]; then  
-        echo "please input your mnemonic"  
-        exit 1  
+        echo "Usage: /bin/bash audit.sh your-mnemonic-phrase satis-logfile"
+        exit 4
     fi
 
     if [ -z "$2" ]; then  
-        echo "please input data"  
-        exit 1  
+        echo "Usage: /bin/bash audit.sh your-mnemonic-phrase satis-logfile"
+        exit 5  
     fi  
 
-    node Web3Password-Tools/web3password-api-audit.js "$mnemonic" "$data"
+    node ${W3PTOOLS_DIR}/web3password-api-audit.js "$mnemonic" "$satis_logfile"
 
     exit 0
 }  
